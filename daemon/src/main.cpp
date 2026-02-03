4f7098b34ddfd9fca11a0c70ea56322bc9a032ce
chanu <chanuka.williams@outlook.com>
2026-02-03 18:21:18 +0000

    Loop continously updates LEDs unless it should stop.

--- daemon/src/main.cpp
+++ daemon/src/main.cpp
@@ -1,11 +1,19 @@
+#include "CommandsListener.h"
+
 #include <SkydimoDriver.h>
 
 int main()
 {
     SkydimoDriver driver("/dev/ttyUSB0", 60);
+    CommandsListener listener("/tmp/openskydimo.sock", driver);
 
     driver.OpenSerialConnection();
-
     driver.Fill(ColorRGB(255, 255, 255));
-    driver.SendColors();
+
+    listener.Start();
+    while (!listener.ShouldStop())
+    {
+        driver.SendColors();
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
 }
