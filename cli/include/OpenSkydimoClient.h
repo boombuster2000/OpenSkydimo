b78db39872aa486afef58cf177542c9d656ab300
chanu <chanuka.williams@outlook.com>
2026-05-18 12:29:30 +0100

    Created specific openskydimo client inherited from UnixSocketClient.

---
+++ cli/include/OpenSkydimoClient.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "openskydimo/types/Response.h"
+
+#include <ipc/UnixSocketClient.h>
+
+class OpenSkydimoClient : public UnixSocketClient
+{
+public:
+    OpenSkydimoClient(std::string socketPath, int bufferSize);
+
+    void SendCommand(int argc, char* argv[]) const;
+    [[nodiscard]] Response GetResponse() const;
+};
