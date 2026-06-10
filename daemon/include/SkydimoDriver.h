36525b41bceaa1c1607e171cd6518d1a548f71df
chanu <chanuka.williams@outlook.com>
2026-06-10 22:02:21 +0100

    Skydimo driver now throws exception.

--- daemon/include/SkydimoDriver.h
+++ daemon/include/SkydimoDriver.h
@@ -1,16 +1,14 @@
 #pragma once
 #include "openskydimo/types/ColorRGB.h"
-
+#include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/spdlog.h"
 #include <mutex>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
-#include "spdlog/sinks/stdout_color_sinks.h"
-#include "spdlog/spdlog.h"
-
 class SkydimoDriver
 {
-
 public:
     SkydimoDriver() = default;
     ~SkydimoDriver();
@@ -19,31 +17,50 @@ public:
     void SetBaudRate(int baudRate);
     void SetLedCount(int ledCount);
 
-    bool OpenSerialConnection();
+    void OpenSerialConnection();
     void CloseSerialConnection();
 
     [[nodiscard]] bool IsReadyToSend() const;
+
     void SendColors() const;
+
     void Fill(ColorRGB color);
 
 private:
     void AddHeaderToBuffer();
 
 private:
-    std::shared_ptr<spdlog::logger> logger =
-        spdlog::get("SkydimoDriver") ? spdlog::get("SkydimoDriver") : spdlog::stdout_color_mt("SkydimoDriver");
-
-    // Thread synchronization - mutable to allow locking in const methods
+    std::shared_ptr<spdlog::logger> logger = spdlog::stdout_color_mt("SkydimoDriver");
     mutable std::mutex m_mutex;
-
     bool m_isReadyToSend = false;
-
     static constexpr int m_headerSize = 6;
     int m_serialPort = -1;
-
     std::string m_portName;
     int m_ledCount = 0;
     int m_baudRate = 115200;
-
     std::vector<std::byte> m_buffer;
+};
+
+class SkydimoException : public std::runtime_error
+{
+public:
+    explicit SkydimoException(const std::string& message) : std::runtime_error(message)
+    {
+    }
+};
+
+class SerialConnectionException : public SkydimoException
+{
+public:
+    explicit SerialConnectionException(const std::string& message) : SkydimoException(message)
+    {
+    }
+};
+
+class SerialWriteException : public SkydimoException
+{
+public:
+    explicit SerialWriteException(const std::string& message) : SkydimoException(message)
+    {
+    }
 };
