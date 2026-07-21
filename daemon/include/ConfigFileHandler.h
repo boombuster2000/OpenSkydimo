a0189530110a05e3b7da933573a9cd65dad63a4b
chanu <chanuka.williams@outlook.com>
2026-07-21 21:50:29 +0100

    config member is now public and created save method.

--- daemon/include/ConfigFileHandler.h
+++ daemon/include/ConfigFileHandler.h
@@ -1,14 +1,15 @@
 #pragma once
-
 #include <filesystem>
 #include <format>
 #include <fstream>
-#include <string>
-
 #include <nlohmann/json.hpp>
+#include <string>
 
 class ConfigFileHandler
 {
+public:
+    nlohmann::json config;
+
 public:
     explicit ConfigFileHandler(const std::string& filepath) : m_filepath(filepath)
     {
@@ -25,7 +26,7 @@ public:
 
         try
         {
-            m_config = nlohmann::json::parse(fileStream);
+            config = nlohmann::json::parse(fileStream);
         }
         catch (const nlohmann::json::parse_error& e)
         {
@@ -33,22 +34,42 @@ public:
         }
     }
 
-private:
-    void InitConfig() const
+    void Save() const
     {
-        std::filesystem::create_directories(m_filepath.parent_path());
+        EnsureDirectoryExists();
 
         std::fstream fileStream(m_filepath, std::ios::out);
+        if (!fileStream.is_open())
+            throw std::runtime_error("Failed to open config file for writing.");
 
+        try
+        {
+            fileStream << config.dump(4);
+        }
+        catch (const nlohmann::json::type_error& e)
+        {
+            throw std::runtime_error(std::format("Failed to serialize config: {}", e.what()));
+        }
+    }
+
+private:
+    void EnsureDirectoryExists() const
+    {
+        std::filesystem::create_directories(m_filepath.parent_path());
+    }
+
+    void InitConfig() const
+    {
+        EnsureDirectoryExists();
+
+        std::fstream fileStream(m_filepath, std::ios::out);
         if (!fileStream.is_open())
             throw std::runtime_error("Failed to create and open config file.");
 
         const nlohmann::json defaultConfig = {{"port", ""}, {"ledCount", 0}, {"lastEffect", nullptr}};
         fileStream << defaultConfig.dump(4);
-        fileStream.close();
     }
 
 private:
     std::filesystem::path m_filepath;
-    nlohmann::json m_config;
 };
